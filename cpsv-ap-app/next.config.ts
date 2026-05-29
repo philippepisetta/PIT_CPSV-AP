553acf4a152d37cb9bb3541ae70419477aa4b3d8
philippepisetta <philippe.pisetta@ext.adn.be>
2026-05-29 17:15:13 +0200

    Update next.config.ts

--- cpsv-ap-app/next.config.ts
+++ cpsv-ap-app/next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3001/api/:path*",
+        destination: `${apiUrl}/api/:path*`,
       },
     ];
   },
